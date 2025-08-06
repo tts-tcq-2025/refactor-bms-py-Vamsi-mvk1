8518eb5ff852a840efe99d9740a8fa8fd1963505
vamsi <vamsimvk1@gmail.com>
2025-08-06 10:30:18 +0530

    Update check_alerts.py

--- check_alerts.py
+++ check_alerts.py
@@ -2,9 +2,9 @@ import sys
 from time import sleep
 
 def blink_alert(duration=6):
-    states = ['\r* ', '\r *']
-    for _ in range(duration):
-        for state in states:
-            print(state, end='')
-            sys.stdout.flush()
-            sleep(1)
+    total_flashes = duration * 2
+    for i in range(total_flashes):
+        state = '\r* ' if i % 2 == 0 else '\r *'
+        print(state, end='')
+        sys.stdout.flush()
+        sleep(1)
