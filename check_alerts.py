7830fdd9a6885e2e9d71f42d91277e317033e9a5
vamsi <vamsimvk1@gmail.com>
2025-08-06 10:37:07 +0530

    Update check_alerts.py

--- check_alerts.py
+++ check_alerts.py
@@ -1,10 +1,23 @@
 import sys
 from time import sleep
-
-def blink_alert(duration=2):
-    total_flashes = duration * 2
-    for i in range(total_flashes):
-        state = '\r* ' if i % 2 == 0 else '\r *'
-        print(state, end='')
+def blink_alert(duration=6, step_duration=1):
+    # step_duration = how long each half-blink lasts (in seconds)
+    total_steps = int(duration / step_duration)
+    states = ['\r* ', '\r *']
+    
+    for i in range(total_steps):
+        print(states[i % 2], end='')
         sys.stdout.flush()
-        sleep(1)
+        sleep(step_duration)
+
+
+
+
+
+# def blink_alert(duration=2):
+#     total_flashes = duration * 2
+#     for i in range(total_flashes):
+#         state = '\r* ' if i % 2 == 0 else '\r *'
+#         print(state, end='')
+#         sys.stdout.flush()
+#         sleep(1)
